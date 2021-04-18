0c683264cd2a8b05a3bf349e9557e2631f4e95f0
Funnystyle <esero@hanmail.net>
2021-04-18 09:31:33 +0900

    #2.0 Displaying Temperature

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "e88539b804130a2cf7fa53d9e71284bd";
 
@@ -12,9 +13,9 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
+    this.setState({ isLoading: false, temp: data.main.temp });
   };
   getLocation = async () => {
     try {
@@ -24,7 +25,6 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -33,7 +33,42 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
+
+// const App = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   // const [location, setLocation] = useState({ latitude: null, longitude: null });
+//   const [temp, setTemp] = useState();
+
+//   const getWeather = async (latitude, longitude) => {
+//     const { data } = await axios.get(
+//       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+//     );
+//     setIsLoading(false);
+//     setTemp(data.main.temp);
+//   };
+//   const getLocation = async () => {
+//     try {
+//       // await Location.requestPermissionsAsync(); // deprecated
+//       await Location.getForegroundPermissionsAsync();
+//       const {
+//         coords: { latitude, longitude },
+//       } = await Location.getCurrentPositionAsync();
+//       getWeather(latitude, longitude);
+//       setIsLoading(false);
+//     } catch (error) {
+//       Alert.alert("Can't find you.", "So sad");
+//     }
+//   };
+
+//   useEffect(() => {
+//     getLocation();
+//   }, []);
+
+//   return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+// };
+
+// export default App;
